81d69073c42058fcbe677f3bcc4d961a4e9c4492
Varun Mulloli <vmulloli@dentolo.de>
2021-08-10 22:46:02 +0100

    bitrate switch events and buffering events

--- index.js
+++ index.js
@@ -29,14 +29,54 @@ var express = require('express'),
     }
   }
 
+  function calculateTooManyBitrateSwitches(bitrateSwitchEvents) {
+    if(Array.isArray(bitrateSwitchEvents) && bitrateSwitchEvents.length > 1) {
+      bitrateSwitchEvents.reverse();
+      const lastSwitchTimestamp = !!bitrateSwitchEvents[0] ? bitrateSwitchEvents[0].timestamp : 0;
+      const validatedTimestamp = !!lastSwitchTimestamp ? lastSwitchTimestamp : Date.now();
+      const tenSecondsBefore = validatedTimestamp - 10000;
+      const eventsInTenSecondRange = bitrateSwitchEvents.filter(event => event.timestamp > tenSecondsBefore);
+      if(eventsInTenSecondRange.length > 2) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  function calculateTooManyBuffering(bufferingEvents) {
+    if(Array.isArray(bufferingEvents) && bufferingEvents.length > 0) {
+      const eventLongerThanOneSecond = bufferingEvents.find(event => event.duration > 1000);
+      if(!!eventLongerThanOneSecond) {
+        return true;
+      } else {
+        bufferingEvents.reverse();
+        const lastSwitchTimestamp = !!bitrateSwitchEvents[0] ? bitrateSwitchEvents[0].timestamp : 0;
+        const validatedTimestamp = !!lastSwitchTimestamp ? lastSwitchTimestamp : Date.now();
+        const thirtySecondsBefore = validatedTimestamp - 30000; 
+        const eventsInThirtySecondRange = bitrateSwitchEvents.filter(event => event.timestamp > thirtySecondsBefore);
+        if(eventsInThirtySecondRange.length > 3) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
   app.post('/event', (req, res) => {
     var body = req.body;
     const highestBitrate = calculateHighestBitrate(body.width, body.height, body.availableTracks);
+    const tooManyBitrateSwitches = calculateTooManyBitrateSwitches(body.bitrateSwitchEvents);
+    const tooManyBuffering = calculateTooManyBuffering(body.bufferingEvents);
     res.send({ 
-      "HIGHEST_BITRATE_POSSIBLE": {
-        "bitrate": highestBitrate,
-        "possible": highestBitrate > body.currentBitrate
-      }
+      "HIGHEST_BITRATE_POSSIBLE": highestBitrate,
+      "TOO_MANY_BITRATE_SWITCHES": tooManyBitrateSwitches,
+      "TOO_MANY_BUFFERING": tooManyBuffering,
     });
   })
   
